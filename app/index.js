1c3c10472c30987518d3fd871f1ba0438907321f
Bruno Wadhy <brunowadhy@gmail.com>
2026-04-04 12:04:27 -0300

    feat: adiciona API Node.js conectada ao MySQL

---
+++ app/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '123456',
+  database: 'labdb'
+});
+
+app.get('/users', (req, res) => {
+  db.query('SELECT * FROM users', (err, results) => {
+    if (err) return res.status(500).send(err);
+    res.json(results);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('App rodando na porta 3000');
+});
